543376439f643f7bc36a92bb08bcb3f2097d5750
benbenlaw <benlawrenceben@hotmail.co.uk>
2024-11-28 14:42:25 +0000

    5.0.16

--- src/main/java/com/benbenlaw/caveopolis/util/CaveopolisColorHandler.java
+++ src/main/java/com/benbenlaw/caveopolis/util/CaveopolisColorHandler.java
@@ -152,7 +152,7 @@ public class CaveopolisColorHandler {
     }
 
     @SubscribeEvent
-    public void onItemColors(RegisterColorHandlersEvent.Item event) {
+        public void onItemColors(RegisterColorHandlersEvent.Item event) {
 
         //Colored Items
         event.register(new IColored.ItemColors(), CaveopolisItems.COLORED_APPLE.get().asItem());
